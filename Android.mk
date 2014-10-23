aa0c1c57130238c400dae4e3865b311168cf493a
Howard M. Harte <hharte@cyngn.com>
2014-10-23 14:43:56 -0700

    Fix typo in CFLAGS
    Change-Id: Ibc04689173689bb288abf7e2226c5545c4347d27

--- Android.mk
+++ Android.mk
@@ -44,7 +44,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := eng
 LOCAL_SRC_FILES := dvenue_dump_images.c
 LOCAL_CFLAGS += -I. -I/usr/include/ -DDVENUE8_3840
-LOCAL_CFLAGS += -I. -I/usr/include/
 LOCAL_MODULE := dvenue3840_dump_images
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_HOST_EXECUTABLE)
